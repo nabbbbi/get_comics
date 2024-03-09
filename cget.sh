6cc91c6652932d8fd53e88cb6e1a8ecbf2c4e436
nabbbbi <nabbbbi@mail.ru>
2024-03-09 11:41:03 +0300

    added

---
+++ cget.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+# script downloads up to 50 images from url to the page of comics.
+# in readcomicsonline.ru site.
+# 1st argument will be url to an issue.
+# f is name of comics
+# s is url from site
+# I is number of issue
+
+# check cmd presence
+test "$1" = "" && { echo "No arguments" ; exit 1; }
+# get 'https://readcomicsonline.ru/comic/what-if-aliens-2024/1' as url
+read url <<< `echo "$1" | sed 's/comic/uploads\/manga/2' | sed -r 's/\//\/chapters\//6'`
+# get 'what-if-aliens-2024' as name via regex
+read name <<< `echo "$url" | sed -r 's/.{42}//' | sed -r 's/\/[^0-9]*\//_/' | sed -r 's/\/.*//'`
+
+# make directory to put comics in
+if test ! -d /home/pi/Pictures/Comics/$name; then
+    mkdir /home/pi/Pictures/Comics/$name
+else
+    echo "Directory \"$name\" already exists!"
+    exit 1
+fi
+
+value=100
+I=1
+while test $I -le $value; do
+    if test "$I" -le "9"; then
+	wget -q --directory-prefix=/home/pi/Pictures/Comics/$name/ "$url/0$I.jpg"
+    else
+	wget -q --directory-prefix=/home/pi/Pictures/Comics/$name/ "$url/$I.jpg"
+    fi
+    I=$((I+1))
+done
